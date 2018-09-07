a3919375b18ee24bf94c3012c08a86c1fbb6ce76
Harry Gueguen <harry.gueguen@gmail.com>
2018-09-07 06:54:10 +0200

    🚧 Adds validation on character records

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,9 @@
 class Character < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :hp, presence: true, numericality: { only_integer: true,
+                                                  greater_than: 79,
+                                                  less_than: 121 }
+  validates :attack_points, presence: true, numericality: { only_integer: true,
+                                                            greater_than: 4,
+                                                            less_than: 16 }
 end
